64e8be00cc3b506b5078614db6ecaecf11a29228
Sharan <sharan@esthenos.com>
2018-08-06 21:20:33 +0530

    removed main.py and updated requirements

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-from app import app
+from app import app,socketio
 from flask_script import Manager,Server
 manager = Manager(app)
-manager.add_command("runserver", Server(
-    use_debugger=True,
-    use_reloader=True,
-    host="0.0.0.0", port=9000))
-
+# manager.add_command("runserver", Server(
+#     use_debugger=True,
+#     use_reloader=True,
+#     host="0.0.0.0", port=9000))
+manager.add_command("runserver", socketio.run(app,debug=True,port=8080))
 if __name__ == '__main__':
     manager.run()

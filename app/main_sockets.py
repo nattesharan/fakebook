937bc2c58f5673e8c36b928b62046ade64e29ef8
Sharan <sharan@esthenos.com>
2018-08-07 19:22:17 +0530

    changed the way of getting userid while diconnecting and sending active users when connection

--- app/main_sockets.py
+++ app/main_sockets.py
@@ -1,15 +1,17 @@
-from flask_socketio import join_room,rooms,leave_room
+from flask_socketio import join_room,rooms,leave_room,emit
 from flask import request
 from fakebook.models import FakeBookUser
+from utils import get_id_from_rooms
 def connect():
+    join_room('main')
     print "Successfully connected"
 
 def disconnect():
     user_rooms = rooms()
     for user_room in user_rooms:
         leave_room(user_room)
-    user_rooms.remove(request.sid)
-    user_id = user_rooms[0]
+    user_id = get_id_from_rooms(user_rooms)
+    print user_id
     user = FakeBookUser.objects.get(id=user_id)
     user.become_offline()
     print "Successfully Disconnected"
@@ -20,3 +22,6 @@ def create_room(data):
     user.become_online()
     if user_id not in rooms():
         join_room(data['user_id'])
+    active_users = FakeBookUser.objects.filter(is_online=True).only('id')
+    active_users = [str(active_user.id) for active_user in active_users]
+    emit('connected_online',{'users':active_users},room='main')
